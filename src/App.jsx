aa0c3c98e5bdfc4fab03c5fd7ac2c848d0983354
Martin Luna <uxmoon@gmail.com>
2022-01-06 20:33:05 -0300

    Added some links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Menu delivery</h1>
+      <nav>
+        <ul>
+          <Link to="/menu">Menu</Link>
+          <Link to="/stores">Stores</Link>
+        </ul>
+      </nav>
     </div>
   );
 }

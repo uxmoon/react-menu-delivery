23ffe91019cf93463d8de65627ca601385802675
Martin Luna <uxmoon@gmail.com>
2022-01-06 20:31:36 -0300

    Added routes

---
+++ src/routes/menu.jsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Menu</h2>
+    </main>
+  );
+}
---
+++ src/routes/stores.jsx
@@ -0,0 +1,7 @@
+export default function Stores() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Stores</h2>
+    </main>
+  );
+}

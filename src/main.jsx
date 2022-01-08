7a5a40ccf8b37b52a842b833776edb0686363438
Martin Luna <uxmoon@gmail.com>
2022-01-07 23:43:10 -0300

    Import Layout to display pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,24 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./routes/menu";
 import Stores from "./routes/stores";
+import Layout from "./Layout";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="menu" element={<Menu />} />
-      <Route path="stores" element={<Stores />} />
-      <Route
-        path="*"
-        element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="stores" element={<Stores />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")

ca470fed6a9ce3e8553348325ecbfd4b2ebb8e5e
Martin Luna <uxmoon@gmail.com>
2022-01-07 23:42:14 -0300

    Remove menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import "./App.css";
-import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Menu delivery</h1>
-      <nav>
-        <ul>
-          <Link to="/menu">Menu</Link>
-          <Link to="/stores">Stores</Link>
-        </ul>
-      </nav>
     </div>
   );
 }
